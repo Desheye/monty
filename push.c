95436beb95e78639972d71b5a54a878551e7719b
Desheye <aarealaafiaboyz@gmail.com>
2023-12-17 19:52:00 +0100

    monty again

--- push.c
+++ push.c
@@ -1,46 +1,37 @@
 #include "monty.h"
-
 /**
  * f_push - add node to the stack
- * @stack_head: stack head
- * @line_number: line_number
+ * @head: stack head
+ * @counter: line_number
  * Return: no return
 */
-void f_push(stack_t **stack_head, unsigned int line_number)
+void f_push(stack_t **head, unsigned int counter)
 {
-    int value, index = 0, invalid_flag = 0;
+	int n, j = 0, flag = 0;
 
-    if (var_global.push_arg)
-    {
-        if (var_global.push_arg[0] == '-')
-            index++;
-        for (; var_global.push_arg[index] != '\0'; index++)
-        {
-            if (var_global.push_arg[index] > '9' || var_global.push_arg[index] < '0')
-                invalid_flag = 1;
-        }
-        if (invalid_flag == 1)
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", line_number);
-            fclose(var_global.file);
-            free(var_global.buffer);
-            free_stack(*stack_head);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        fclose(var_global.file);
-        free(var_global.buffer);
-        free_stack(*stack_head);
-        exit(EXIT_FAILURE);
-    }
-
-    value = atoi(var_global.push_arg);
-    if (var_global.lifo == 0)
-        add_node_to_stack(stack_head, value);
-    else
-        add_node_to_queue(stack_head, value);
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
-
